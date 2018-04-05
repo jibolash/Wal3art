f2dca1b9349aff5365e1981e6d0d4c6a36f2dcdf
Jibola Sogbein <ajibolasogbein@gmail.com>
2018-04-04 20:48:25 -0500

    refactor shoppingcategory activity

--- app/src/main/java/com/mayowasogbein/android/walmart/ShoppingCategory.java
+++ app/src/main/java/com/mayowasogbein/android/walmart/ShoppingCategory.java
@@ -7,7 +7,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ShoppingCategory extends AppCompatActivity {
+public class ShoppingCategory extends AppCompatActivity implements View.OnClickListener {
     private TextView welcomeMessageTextView;
     private LinearLayout beauty, food, clothing, electronics;
 
@@ -21,62 +21,35 @@ public class ShoppingCategory extends AppCompatActivity {
         welcomeMessageTextView = findViewById(R.id.welcomeMessage);
 
         beauty = findViewById(R.id.beauty);
+        beauty.setOnClickListener(this);
         food = findViewById(R.id.food);
+        food.setOnClickListener(this);
         clothing = findViewById(R.id.clothing);
+        clothing.setOnClickListener(this);
         electronics = findViewById(R.id.electronics);
+        electronics.setOnClickListener(this);
 
         welcomeMessageTextView.setText("Welcome " + username);
+    }
 
+    @Override
+    public void onClick(View view) {
+        String categoryName = "";
+        switch (view.getId()) {
+            case R.id.beauty:
+                categoryName = "Beauty";
+                break;
+            case R.id.food:
+                categoryName = "Food";
+                break;
+            case R.id.clothing:
+                categoryName = "Clothing";
+                break;
+            case R.id.electronics:
+                categoryName = "Electronics";
+                break;
+        }
+        Toast.makeText(getApplicationContext(), "You have chosen the "+ categoryName +" category of shopping", Toast.LENGTH_SHORT).show();
 
-        beauty.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                String categoryName = "";
-                if(view.getId() == R.id.beauty) {
-                    categoryName = "Beauty";
-                }
-                Toast.makeText(getApplicationContext(), "You have chosen the " + categoryName + " category of shopping", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        food.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                String categoryName = "";
-                if(view.getId() == R.id.food) {
-                    categoryName = "Food";
-                }
-                Toast.makeText(getApplicationContext(), "You have chosen the " + categoryName + " category of shopping", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        clothing.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                String categoryName = "";
-                if(view.getId() == R.id.clothing) {
-                    categoryName = "Clothing";
-                }
-                Toast.makeText(getApplicationContext(), "You have chosen the " + categoryName + " category of shopping", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        electronics.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                String categoryName = "";
-                if(view.getId() == R.id.electronics) {
-                    categoryName = "Electronics";
-                }
-                Toast.makeText(getApplicationContext(), "You have chosen the " + categoryName + " category of shopping", Toast.LENGTH_SHORT).show();
-
-            }
-        });
     }
 }
